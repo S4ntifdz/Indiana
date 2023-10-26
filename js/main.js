88e21bc603ed9788c767177a31f5392df385963c
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2023-10-26 02:01:28 -0300

    update index.html & main.js
    - added select element to choose language
    - added data-translate
    - JS search in json files and do the translations

--- js/main.js
+++ js/main.js
@@ -7,7 +7,30 @@ window.addEventListener('DOMContentLoaded', function () {
     modal.showModal()
 
     closeModalButton.addEventListener('click', function () {
+        modal.style.visibility = 'hidden'
+        modal.style.opacity = 0
         modal.close()
     })
     /* END - MODAL */
+
+    /* BEGIN - TRANSLATIONS */
+    const chooseLanguage = document.getElementById('id_change_language')
+    chooseLanguage.addEventListener('change', changeLanguage)
+
+    function changeLanguage() {
+        currentLanguage = chooseLanguage.value;
+        path = 'i18n/' + currentLanguage + '.json'
+
+        fetch(path)
+            .then((response) => response.json())
+            .then((translations) => {
+                const elements = document.querySelectorAll('[data-translate]')
+                elements.forEach((element) => {
+                    const key = element.getAttribute('data-translate')
+                    element.textContent = translations[key]
+                })
+            })
+    }
+    /* END - TRANSLATIONS */
+
 })
