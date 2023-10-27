0639e9e5cad1272d6b61d19f114ad3ccf0dad5a6
Roman Arroyo <arroyoroman32@gmail.com>
2023-10-26 22:25:08 -0300

    mailto: funcional

---
+++ js/mailto.js
@@ -0,0 +1,10 @@
+function mailto_link(){
+    var contactname = document.getElementById('contact-name').value;
+    var contactemail = document.getElementById('contact-email').value;
+    var contactcompany = document.getElementById('contact-name').value;
+    var message = document.getElementById('contact-name').value;
+    var body = "Nombre completo:"+ contactname + "        Correo de contacto:"+ contactemail + "        compañia" + contactcompany + "        mensaje:"+ message
+
+    var mailto_link='mailto:emaildeejemplo@gmail.com'+'?subject=Datos de contacto'+'&body='+body
+    win=window.open(mailto_link,'emailWin');
+}
